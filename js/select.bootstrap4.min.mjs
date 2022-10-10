15f6bd35025b4046b47664e9bf7b29cad770c800
Allan Jardine <allan@datatables.net>
2022-10-10 19:51:33 +0000

    Add support for mjs files (ES Modules)

---
+++ js/select.bootstrap4.min.mjs
@@ -0,0 +1,1 @@
+import $ from"jquery";import DataTable from"datatables.net-bs4";import DataTable from"datatables.net-select";export default DataTable;
---
+++ js/select.bootstrap4.mjs
@@ -0,0 +1,13 @@
+
+/*! Bootstrap 4 styling wrapper for Select
+ * © SpryMedia Ltd - datatables.net/license
+ */
+
+import $ from 'jquery';
+import DataTable from 'datatables.net-bs4';
+import DataTable from 'datatables.net-select';
+
+
+
+
+export default DataTable;
