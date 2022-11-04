2bc22b3ca650bd3c8c880f4822e60659c1ab087b
Allan Jardine <allan@datatables.net>
2022-11-04 14:54:44 +0000

    Missing files in DataTables-DataTables

--- js/select.bootstrap4.min.mjs
+++ js/select.bootstrap4.min.mjs
@@ -1,4 +1,1 @@
-/*! Bootstrap 4 styling wrapper for Select
- * © SpryMedia Ltd - datatables.net/license
- */
 import $ from"jquery";import DataTable from"datatables.net-bs4";import DataTable from"datatables.net-select";export default DataTable;
