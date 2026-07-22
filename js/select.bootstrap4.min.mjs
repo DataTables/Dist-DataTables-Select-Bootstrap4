bec3a2cb32d7f5135e3b5e5c81a832e220623644
dtbuild <dtbuild@rigil>
2026-07-22 14:55:46 +0000

    2f187333be7f60b09ce6644db27cfeb2c595077a Release 4.0.0 ready
    Sync to source repo @2f187333be7f60b09ce6644db27cfeb2c595077a

--- js/select.bootstrap4.min.mjs
+++ js/select.bootstrap4.min.mjs
@@ -1,4 +1,4 @@
-/*! Select Bootstrap 4 styling 4.0.0-beta.1 for DataTables
+/*! Select Bootstrap 4 styling 4.0.0 for DataTables
  * Copyright (c) SpryMedia Ltd - datatables.net/license
  */
 import DataTable from"datatables.net-bs4";import select from"datatables.net-select";export default DataTable;
